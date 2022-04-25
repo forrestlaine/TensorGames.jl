364a382c44165f6758bf19ff887d141a03a1b349
lassepe <lasse.peters@mailbox.org>
2022-04-25 21:51:12 +0200

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,6 @@ using TensorGames
 using LinearAlgebra: norm
 using Zygote: gradient, forwarddiff
 using Random: MersenneTwister
-using FiniteDifferences: to_vec
 
 include("test_equilibrium.jl")
 include("test_derivatives.jl")
